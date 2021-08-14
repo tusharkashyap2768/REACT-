6e0c64959f1a54036ed598e97970e283dfb63e60
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-08-14 13:22:43 +0530

    Add files via upload

---
+++ 1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./style.css"
+import Button from "./Button"
+const App = () => {
+    return (<>
+        <h1 style={{textAlign: "center"}}> !!</h1>
+        <Button title="APP STORE"/>
+        <Button title="PLAY STORE"/>
+        {/* <button className="button">hey ! click here</button>
+        <button className="button">hey ! click here</button> */}
+        </>
+    )
+}
+
+export default App
---
+++ 1/src/Button.js
@@ -0,0 +1,7 @@
+import React from 'react'
+const Button = ({title}) => (
+    <div>
+    <button className="button">{title}</button>
+    </div>
+);
+export default Button;
